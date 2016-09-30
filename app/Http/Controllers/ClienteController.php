ae322e545a1e8e0926ba4b22674b853f7278ee89
Redlein <redlein7@gmail.com>
2016-09-30 17:25:20 -0500

    Modificando Routing

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Cliente;
+
+class ClienteController extends Controller
+{
+    public function index()
+    {
+        $data = Cliente::all();
+
+        return response()->json($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Cliente($request->all());
+        $data->save();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = Cliente::find($id);
+        $data->fill($request->all());
+        $data->save();
+
+        return $this->index();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Cliente::destroy($id);
+    }
+}
--- public/angular/main.js
+++ public/angular/main.js
@@ -1,47 +1,13 @@
-/**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- */
 
-/**
- * Main AngularJS Web Application
- */
 var app = angular.module('App', [
   'ngRoute'
 ]);
 
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider
-    // Home
-    .when("/", {templateUrl: "view/inicio.html", controller: "panelController"})
-   
-    // Pages
-  
-    .when("/Reporte", {templateUrl: "partials/admin/reporte/reportes.html", controller: "PageCtrl"})
-    // else 404
-    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $routeProvider.when("/", {templateUrl: "view/inicio.html"})
+    $routeProvider.when("/hola", {templateUrl: "view/2.html"})
+    $routeProvider.otherwise("/404", {templateUrl: "partials/404.html"});
+
+    $locationProvider.html5Mode({ enabled: true, requireBase: false });
 }]);
-
-/**
- * Controls all other Pages
- */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty(reconocio el controlador).");
-
-});
-app.controller('panelController', function($scope, $http) {
-
-    $http.get('admin/panel').then(function successCallback(response) {
-            window.location.href = 'admin#/' + response.data.mensaje;
-        }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        });
-    
-
-});
-
-
